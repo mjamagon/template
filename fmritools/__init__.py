a5c074cf2a42720ae879f6696435b7c9b3a76f5a
Sam Zorowitz <szorowi1@gmail.com>
2019-01-20 00:43:21 -0500

    initial upload of materials

---
+++ fmritools/__init__.py
@@ -0,0 +1,7 @@
+"""Useful tools for fMRI analysis"""
+
+__version__ = '0.1'
+
+from . import hrf
+from . import design
+from .nuisance import (legendre)
---
+++ fmritools/design/experiment.py
@@ -0,0 +1,8 @@
+import numpy as np
+from ..hrf import spm_hrf
+
+def design_matrix():
+    print('Ready to write some code?')
+    
+def fir_matrix():
+    print('Ready to write some code?')
